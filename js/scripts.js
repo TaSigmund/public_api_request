8a86f6858b6718ca55559097ee0708e2b4feccec
TaSigmund <64551452+TaSigmund@users.noreply.github.com>
2020-10-15 20:36:36 +0200

    html is generated

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
-fetch('https://randomuser.me/api/?results=12')
-  .then(response => response.json())
-  .then(data => data.results)
-  .then(results => results.map(result => 
-    console.log(result.name.first + ' ' + result.name.last + ' ' + result.email  + ' ' + result.location.street.number + ' ' + result.location.street.name + ' ' + result.location.postcode + ' ' +  result.location.city + ' ' + result.location.state + ' ' + result.cell + ' ' + result.dob.date)
+const gallery = document.getElementById('gallery');
+
+fetch('https://randomuser.me/api/?results=12&nat=us') //returns the data for twelve users of us origin
+  .then(response => response.json()) //turns the returned string into json
+  .then(data => data.results) //retrieves the results object
+  .then(results => results.map(result => //iterates over the array of user objects
+    gallery.insertAdjacentHTML('beforeend', 
+        `<div class="card">
+        <div class="card-img-container">
+        <img class="card-img" src="${result.picture.large}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+        <h3 id="name" class="card-name cap">${result.name.first} ${result.name.last}</h3>
+        <p class="card-text">${result.email}</p>
+        <p class="card-text cap">${result.location.city}, ${result.location.state}</p>
+        </div>
+        </div>`)
     ))
+
+    //console.log(result.name.first + ' ' + result.name.last + ' ' + result.email  + ' ' + result.location.street.number + ' ' + result.location.street.name + ' ' + result.location.postcode + ' ' +  result.location.city + ' ' + result.location.state + ' ' + result.cell + ' ' + result.dob.date + ' ' + result.picture.large)

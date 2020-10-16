a3bccff5cc9e801349e508392ca93c7d4cb2af73
TaSigmund <64551452+TaSigmund@users.noreply.github.com>
2020-10-16 16:22:55 +0200

    refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,39 @@
+const url = 'https://randomuser.me/api/?results=12&nat=us';
 const gallery = document.getElementById('gallery');
+const body = document.getElementsByTagName('body')[0];
 
-fetch('https://randomuser.me/api/?results=12&nat=us') //returns the data for twelve users of us origin
-  .then(response => response.json()) //turns the returned string into json
-  .then(data => data.results) //retrieves the results object
-  .then(results => results.map(result => //iterates over the array of user objects
-    gallery.insertAdjacentHTML('beforeend', 
+function generateGallery(people){
+    people.map(
+        person => {//iterates over the array of user objects
+        gallery.insertAdjacentHTML('beforeend', 
         `<div class="card">
         <div class="card-img-container">
-        <img class="card-img" src="${result.picture.large}" alt="profile picture">
+        <img class="card-img" src="${person.picture.large}" alt="profile picture">
         </div>
         <div class="card-info-container">
-        <h3 id="name" class="card-name cap">${result.name.first} ${result.name.last}</h3>
-        <p class="card-text">${result.email}</p>
-        <p class="card-text cap">${result.location.city}, ${result.location.state}</p>
+        <h3 id="${person.name.first}${person.name.last}" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+        <p class="card-text">${person.email}</p>
+        <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
         </div>
-        </div>`)
-    ))
+        </div>`);
+    })
+}
+
+fetch(url) //returns the data for twelve users of us origin
+  .then(response => response.json()) //turns the returned string into json
+  .then(data => generateGallery(data.results)) //uses the results object to generate the HTML for the gallery
+    
+
+
+    /**gallery.addEventListener('click', (e) => {
+        if (e.target.className.includes('card')){
+            console.log('card')
+            }
+        })***/
+    
+
+   /*** ***/
+
+    
 
     //console.log(result.name.first + ' ' + result.name.last + ' ' + result.email  + ' ' + result.location.street.number + ' ' + result.location.street.name + ' ' + result.location.postcode + ' ' +  result.location.city + ' ' + result.location.state + ' ' + result.cell + ' ' + result.dob.date + ' ' + result.picture.large)
